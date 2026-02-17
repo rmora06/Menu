0d30b4e21b35eac1f8e87f7355ab5588e52f8347
rmora06 <robertjouelmora@gmail.com>
2026-02-16 20:18:25 -0400

    side menu

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -35,3 +35,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+const overlay = document.getElementById("overlay");
+
+hamburger.addEventListener("click", () => {
+    mobileMenu.classList.add("active");
+    overlay.classList.add("active");
+    hamburger.classList.add("hidden");
+});
+
+overlay.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    overlay.classList.remove("active");
+    hamburger.classList.remove("hidden");
+});
+
+
+document.querySelectorAll(".mobile-nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        mobileMenu.classList.remove("active");
+        overlay.classList.remove("active");
+        hamburger.classList.remove("hidden");
+    });
+});
+
